273bc47e1b50745a6fb97b4685d21539b1c547a3
João Pedro <jjoao.monteiro15@gmail.com>
2022-08-06 21:44:40 -0300

    feat: organized infos in context with a second request in pokeApi

--- src/context/index.tsx
+++ src/context/index.tsx
@@ -1,4 +1,6 @@
-import { createContext, useState, useEffect, ReactElement } from "react"
+import React, { createContext, useState, useEffect, ReactElement } from "react"
+import { API } from "../services/api";
+
 import { usePokemon } from '../services/hooks/usePokemon'
 
 interface ContextType {
@@ -14,16 +16,26 @@ interface InfoProps {
   name: string;
   id: string | null;
 }
-export function PokemonData({ children }: Props) {
-  const { pokemonData, pokemonSuccess } = usePokemon(0, 20)
-  const [dataPokemon, setDataPokemon] = useState<any>(null)
+export function PokemonProvider({ children }: Props) {
+  const { pokemonData, pokemonSuccess } = usePokemon()
+  const [pokemons, setPokemons] = useState<any>([])
 
   useEffect(() => {
-    setDataPokemon(pokemonData)
-  }, [pokemonSuccess])
+    if(pokemonSuccess) {
+        const arrayPokemons : any = []
+        pokemonData.results.map(async (pokemon:{name:string, url:string}) => {
+            const { data }  = await API.get(pokemon.url)
+            arrayPokemons.push(data)            
+        } )            
+        setPokemons(arrayPokemons)        
+    }
+}, [pokemonSuccess])
+
+
+
 
   return (
-    <PokemonContext.Provider value={{ dataPokemon }}>
+    <PokemonContext.Provider value={{ pokemons }}>
       {children}</PokemonContext.Provider>
   )
 }
