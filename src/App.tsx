57e739dfe7dbc0c138da9b644736e81264e2dd92
João Pedro <jjoao.monteiro15@gmail.com>
2022-08-06 21:45:17 -0300

    feat: call to Context and getting data pokemons

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,12 @@
-import * as React from 'react'
+import React, { useContext, useEffect } from 'react'
+import { PokemonContext } from './context'
 
 function App() {
+  const { pokemons } = useContext(PokemonContext)
+  useEffect(() => {
+    console.log(pokemons, 'poke');
+    
+  }, [pokemons])
   return (
     <div className="App">
       <h1>Hello World</h1>
