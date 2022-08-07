2bd032ccb6124302052675d6c77909d73d370352
João Pedro <jjoao.monteiro15@gmail.com>
2022-08-06 21:43:20 -0300

    chore: define limit and offSet in params default value

--- src/services/hooks/usePokemon.tsx
+++ src/services/hooks/usePokemon.tsx
@@ -1,13 +1,15 @@
 import { API } from '../api'
 import { useQuery } from 'react-query'
+import { useEffect, useState } from 'react'
 
-const getPokemon = async (offSet: number = 0, limit: number = 100) => {
+const getPokemon = async (offSet: number, limit: number) => {
     const { data } = await API.get(`/pokemon/?offset=${offSet}&limit=${limit}`) 
     return data
 }
 
-export const usePokemon = (offSet: number, limit: number) => {
+export const usePokemon = (offSet: number = 0, limit: number = 10) => {
     const { data, isError, isSuccess, isLoading } = useQuery<any>(['pokemon', offSet, limit], async () => await getPokemon(offSet, limit) )
+        
     return {
         pokemonData: data,
         pokemonSuccess: isSuccess,
