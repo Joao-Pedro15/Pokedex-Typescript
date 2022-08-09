199e7798b755711e5ba0ebd06d729e0723cadf47
João Pedro <jjoao.monteiro15@gmail.com>
2022-08-08 22:20:20 -0300

    feat: crate a cardContainer and mapping cardPokemons

---
+++ src/components/CardsContainer/index.tsx
@@ -0,0 +1,27 @@
+import React, { useEffect, useContext } from 'react'
+import { PokemonContext } from '../../context'
+import { CardPokemon } from '../CardPokemon'
+import * as S from './style'
+
+export const CardsContainer = () => {
+    const { pokemons, pokemonLoading } = useContext(PokemonContext)
+    useEffect(() => {
+        console.log(pokemonLoading)
+        
+        console.log(pokemons, 'poke');
+    }, [pokemons])
+    return (
+        <S.Container>
+            { pokemonLoading && (
+                <>
+                    <h1>Porra se fuder</h1>
+                </>
+            ) }
+            { pokemons.map((pokemon:any) => (
+                <CardPokemon pokemon={pokemon} key={pokemon.id} />
+            )) }
+        </S.Container>
+    )
+}
+
+
