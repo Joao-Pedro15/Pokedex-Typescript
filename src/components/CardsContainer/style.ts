c71d415eb81db4713b7ae49e46113a824e074b1a
João Pedro <jjoao.monteiro15@gmail.com>
2022-08-10 22:16:50 -0300

    style: create a grid content

--- src/components/CardsContainer/style.ts
+++ src/components/CardsContainer/style.ts
@@ -2,5 +2,8 @@ import styled from "styled-components";
 
 export const Container = styled.div`
     display: grid;
-    background: red;
+    grid-template: 1fr 1fr / 1fr 1fr 1fr 1fr;
+    gap: 20px;
+    padding: 20px;
+    background: linear-gradient(0deg, cyan, red);
 `
