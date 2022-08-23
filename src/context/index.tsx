e6d8a1994d83b7cf2362bf38fa17eb7c86137e72
João Pedro <joao.monteiro@veredaeducacao.com>
2022-08-23 16:30:16 -0300

    chore: change logical requests data pokemon out in context

--- src/context/index.tsx
+++ src/context/index.tsx
@@ -18,24 +18,11 @@ interface InfoProps {
 }
 export function PokemonProvider({ children }: Props) {
   const { pokemonData, pokemonSuccess, pokemonLoading } = usePokemon()
-  const [pokemons, setPokemons] = useState<any>([])
-
-  useEffect(() => {
-    if(pokemonSuccess) {
-        const arrayPokemons : any = []
-        pokemonData.results.map(async (pokemon:{name:string, url:string}) => {
-            const { data }  = await API.get(pokemon.url)
-            arrayPokemons.push(data)            
-        } )            
-        setPokemons(arrayPokemons)        
-    }
-}, [pokemonSuccess])
-
 
 
 
   return (
-    <PokemonContext.Provider value={{ pokemons, pokemonLoading }}>
+    <PokemonContext.Provider value={{ pokemonData, pokemonLoading, pokemonSuccess }}>
       {children}</PokemonContext.Provider>
   )
 }
