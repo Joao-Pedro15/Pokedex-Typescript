53144c5d82fd6af63d6c206b51ec14ce9e280cc6
João Pedro <joao.monteiro@veredaeducacao.com>
2022-08-23 16:33:11 -0300

    feat: do the second request from pokeApi and render data in component CardPokemon

--- src/components/CardPokemon/index.tsx
+++ src/components/CardPokemon/index.tsx
@@ -1,28 +1,44 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { API } from "../../services/api";
 import * as S from './style'
 
-type ICardPokemon = {
-    pokemon: {
-        name: string
-        sprites: {
-            back_default: string
-            front_default: string
-        }
-    } 
-}
+type ICardPokemon = {   
+    name: string
+    sprites: {
+        back_default: string
+        front_default: string
+    }
+} 
 
-export const CardPokemon = ({ pokemon } : ICardPokemon) => {
+export const CardPokemon =  ({ pokemon } : any) => {
     const [flipped, setFlipped] = useState<boolean>(false)
+    const [pokemonData, setPokemonData] = useState<ICardPokemon | null>(null)
+
+    async function getData(){
+        const { data } = await API.get(pokemon.url)
+        return setPokemonData(data)
+    }
+
+    useEffect(() => {
+        getData()
+    }, [])
+
     return(
         <S.Container onClick={() => setFlipped((prev) => !prev)} flipped={flipped}>
+            { !pokemonData ? (
+                <h1>Carregando</h1>
+            ) : (
+            <>
             <div className="card_front">
-                <p>{pokemon.name}</p>
-                <img src={pokemon.sprites.front_default} alt={`image ${pokemon.name}`} />
+                <p>{pokemonData!.name}</p>
+                <img src={pokemonData.sprites.front_default} alt={`image ${pokemon.name}`} />
             </div>
             <div className="card_back">
-                <p>{pokemon.name}</p>
-                <img src={pokemon.sprites.back_default} alt={`image ${pokemon.name}`} />
+                <p>{pokemonData.name}</p>
+                <img src={pokemonData.sprites.back_default} alt={`image ${pokemon.name}`} />
             </div>
+            </>
+            )}
         </S.Container>
     )
 }
