385aaaafd258cb0adc4505b9bfccd2cc092c6e0c
João Pedro <jjoao.monteiro15@gmail.com>
2022-09-01 22:09:19 -0300

    feat: create a component searchInput

---
+++ src/components/SearchInput/style.ts
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    padding: 10px;
+    align-self: flex-start;
+    width: 40%;
+    display: flex;
+    flex-direction: column;
+    input {
+        align-self: flex-end;
+        min-width: 73%;
+        padding: 8px 10px;
+        border-radius: 5px;
+        border: none;
+        outline: none;
+        box-shadow: 3px 4px 15px rgba(0,0,0,.5);
+    }
+`
