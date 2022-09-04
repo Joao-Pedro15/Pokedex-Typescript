bfedee71352ee70760716eeb87b37f1758f98a6f
João Pedro <jjoao.monteiro15@gmail.com>
2022-09-03 22:28:04 -0300

    feat: adding name param in hook getting api endpoint

--- src/services/hooks/usePokemon.tsx
+++ src/services/hooks/usePokemon.tsx
@@ -2,13 +2,13 @@ import { API } from '../api'
 import { useQuery } from 'react-query'
 import { useEffect, useState } from 'react'
 
-const getPokemon = async (offSet: number, limit: number) => {
-    const { data } = await API.get(`/pokemon/?offset=${offSet}&limit=${limit}`) 
+const getPokemon = async (offSet: number, limit: number, name: string | undefined | null) => {
+    const { data } = await API.get(`/pokemon/${name}?offset=${offSet}&limit=${limit}?`) 
     return data
 }
 
-export const usePokemon = (offSet: number = 0, limit: number = 10) => {
-    const { data, isError, isSuccess, isLoading } = useQuery<any>(['pokemon', offSet, limit], async () => await getPokemon(offSet, limit) )
+export const usePokemon = (offSet: number = 0, limit: number = 10, name: string | undefined = '') => {
+    const { data, isError, isSuccess, isLoading } = useQuery<any>(['pokemon', offSet, limit, name], async () => await getPokemon(offSet, limit, name) )
         
     return {
         pokemonData: data,
