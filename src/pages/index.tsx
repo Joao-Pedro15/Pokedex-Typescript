4db5add884979ae37ca8c913d444af608432a753
João Pedro <jjoao.monteiro15@gmail.com>
2022-09-03 22:27:47 -0300

    chore: set setState in props SearchInput

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
 import * as S from './style'
 import { CardsContainer } from '../components/CardsContainer/index'
 import { SearchInput } from '../components/SearchInput'
 
 export const Homepage = () => {
+  const [searchInput, setSearchInput] = useState('')
     return(
       <S.Container>
-        <SearchInput />
+        <SearchInput setInputState={setSearchInput} />
         <CardsContainer />
       </S.Container>
     )
