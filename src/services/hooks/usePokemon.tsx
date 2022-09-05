5e949a4a0898513363eba506a69ecbbd26deb82c
João Pedro <jjoao.monteiro15@gmail.com>
2022-09-04 22:38:10 -0300

    chore: change request for useInfiniteQuery referring a InfiniteScroll component

--- src/services/hooks/usePokemon.tsx
+++ src/services/hooks/usePokemon.tsx
@@ -1,19 +1,28 @@
 import { API } from '../api'
-import { useQuery } from 'react-query'
+import { useQuery, useInfiniteQuery } from 'react-query'
 import { useEffect, useState } from 'react'
 
 const getPokemon = async (offSet: number, limit: number, name: string | undefined | null) => {
-    const { data } = await API.get(`/pokemon/${name}?offset=${offSet}&limit=${limit}?`) 
+    const { data } = await API.get(`/pokemon/${name}?offset=${offSet*60}&limit=${limit}?`) 
     return data
 }
 
 export const usePokemon = (offSet: number = 0, limit: number = 10, name: string | undefined = '') => {
-    const { data, isError, isSuccess, isLoading } = useQuery<any>(['pokemon', offSet, limit, name], async () => await getPokemon(offSet, limit, name) )
+
+    const { data, isError, isSuccess, isLoading, fetchNextPage } = useInfiniteQuery(['pokemon', offSet, limit, name], async ({ pageParam = 0 }) => await getPokemon(pageParam, limit, name), {
+        
+        getNextPageParam: (lastPage, pages) => {
+
+            if(lastPage.length < 60) return undefined
+            return pages.length
+        }
+    })
         
     return {
         pokemonData: data,
         pokemonSuccess: isSuccess,
         pokemonError: isError,
-        pokemonLoading: isLoading
+        pokemonLoading: isLoading,
+        fetchNextPage
     }
 }
