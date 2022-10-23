ea85a42ec9781e7a1450cfffdb2367f829218989
João Pedro <jjoao.monteiro15@gmail.com>
2022-10-23 19:32:48 -0300

    feat: create a component header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,13 @@
 import React from 'react'
 import * as S from './style'
-import HeaderTop from '../../assets/headerTop.png'
 import Pokeball from '../../assets/pokeball.png'
 
 export const Header : React.FC = () => {
   return(
     <S.Container>
-      <img src={HeaderTop} alt="header image" />
       <div>
         <img src={Pokeball} alt="pokeball image" />
+        <h1>Poke<span>Api</span></h1>
       </div>
     </S.Container>
   )
