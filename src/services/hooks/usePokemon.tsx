c0172350a1f0806a993615585ecc55a1bbe11058
João Pedro <jjoao.monteiro15@gmail.com>
2022-10-26 17:15:28 -0300

    Revert "chore: change hook from usePokemon get pokeapi"
    This reverts commit ae8ff52016feff8800610aa416a650e10e295130.

--- src/services/hooks/usePokemon.tsx
+++ src/services/hooks/usePokemon.tsx
@@ -2,14 +2,14 @@ import { API } from '../api'
 import { useQuery, useInfiniteQuery } from 'react-query'
 import { useEffect, useState } from 'react'
 
-const getPokemon = async (url:string) => {
-    const { data } = await API.get(`/pokemon/${url}`) 
+const getPokemon = async (offSet: number, limit: number, name: string | undefined | null) => {
+    const { data } = await API.get(`/pokemon/${name}?offset=${offSet*60}&limit=${limit}?`) 
     return data
 }
 
-export const usePokemon = (url:string) => {
+export const usePokemon = (offSet: number = 0, limit: number = 10, name: string | undefined = '') => {
 
-    const { data, isError, isSuccess, isLoading, fetchNextPage } = useInfiniteQuery(['pokemon', url], async ({ pageParam = 0 }) => await getPokemon(url), {
+    const { data, isError, isSuccess, isLoading, fetchNextPage } = useInfiniteQuery(['pokemon', offSet, limit, name], async ({ pageParam = 0 }) => await getPokemon(pageParam, limit, name), {
         
         getNextPageParam: (lastPage, pages) => {
 
