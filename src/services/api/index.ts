60fd409448910d1e0f60fca819f59a29eb6163eb
João Pedro <jjoao.monteiro15@gmail.com>
2023-04-10 08:49:33 -0300

    feat: create a api2 from testing merge

--- src/services/api/index.ts
+++ src/services/api/index.ts
@@ -3,3 +3,7 @@ import axios from 'axios'
 export const API = axios.create({
     baseURL: 'https://pokeapi.co/api/v2'
 })
+
+export const AP2 = axios.create({
+    baseURL: 'https://pokeapi.co/api/v2'
+})
